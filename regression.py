51a2ff9f97eb4d70203097567b4340eea919a905
Matthew Leech <68862296+MatthewJLeech@users.noreply.github.com>
2020-08-05 21:32:10 -0600

    Add files via upload

---
+++ regression.py
@@ -0,0 +1,145 @@
+# %% read data
+import pandas as pd
+
+train = pd.read_csv(
+    "house-prices-advanced-regression-techniques/train.csv"
+)
+test = pd.read_csv(
+    "house-prices-advanced-regression-techniques/test.csv"
+)
+
+
+# %% checkout out first few rows
+train.head()
+
+
+# %% checkout out dataframe info
+train.info()
+
+
+# %% describe the dataframe
+train.describe(include="all")
+
+
+# %% SalePrice distribution
+import seaborn as sns
+
+sns.distplot(train["SalePrice"])
+
+
+# %% SalePrice distribution with respect to CentralAir / OverallQual / BldgType / etc
+
+sns.boxplot(x="CentralAir", y="SalePrice", data=train)
+
+# %% SalePrice distribution w.r.t YearBuilt / Neighborhood
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(16,8))
+ax = sns.boxplot(x="Neighborhood", y="SalePrice", data=train)
+ax.set_xticklabels(
+    ax.get_xticklabels(), rotation=45
+)
+
+# %%running the same above, substituting Neighborhood for Years and reducing number shown
+
+plt.figure(figsize=(16,8))
+ax = sns.boxplot(x="YearBuilt", y="SalePrice", data=train)
+ax.set_xticklabels(
+    ax.get_xticklabels(), rotation=45, fontsize=5
+)
+
+
+# %%
+from sklearn.dummy import DummyRegressor
+from sklearn.metrics import mean_squared_log_error
+import numpy as np
+
+
+def evaluate(reg, x, y):
+    pred = reg.predict(x)
+    result = np.sqrt(mean_squared_log_error(y, pred))
+    return f"RMSLE score: {result:.3f}"
+
+
+dummy_reg = DummyRegressor()
+
+dummy_selected_columns = ["MSSubClass"]
+dummy_train_x = train[dummy_selected_columns]
+dummy_train_y = train["SalePrice"]
+
+dummy_reg.fit(dummy_train_x, dummy_train_y)
+print("Training Set Performance")
+print(evaluate(dummy_reg, dummy_train_x, dummy_train_y))
+
+truth = pd.read_csv("truth_house_prices.csv")
+dummy_test_x = test[dummy_selected_columns]
+dummy_test_y = truth["SalePrice"]
+
+print("Test Set Performance")
+print(evaluate(dummy_reg, dummy_test_x, dummy_test_y))
+
+print("Can you do better than a dummy regressor?")
+
+
+# %% your solution to the regression problem
+
+from sklearn.linear_model import LinearRegression
+
+reg = LinearRegression()
+
+selected_columns = [
+    "1stFlrSF",
+    "2ndFlrSF", 
+    "Neighborhood", 
+    "BsmtFullBath", 
+    "FullBath"
+]
+
+train_x = train[selected_columns]
+train_y = train["SalePrice"]
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+
+imp = SimpleImputer()
+enc = OneHotEncoder(handle_unknown='ignore')
+ct = ColumnTransformer(
+    [
+        ("Neighborhood_ohe", enc, ["Neighborhood"]),
+        ("fill_na", 
+        imp,
+            [
+            "1stFlrSF",
+            "2ndFlrSF", 
+            "BsmtFullBath", 
+            "FullBath",
+            ],
+        ),
+    ],
+    remainder="passthrough"
+)
+
+train_x = ct.fit_transform(train_x)
+
+reg.fit(train_x, train_y)
+print("Training Set Performance")
+print(evaluate(reg, train_x, train_y))
+
+print("I thiink I did better than a dummy regressor!")
+
+# %%
+#dont use truth in variable for when you do it
+truth = pd.read_csv("truth_house_prices.csv")
+test_x = test[selected_columns]
+test_y = truth["SalePrice"]
+
+test_x = ct.fit_transform(test_x)
+
+print("Test Set Performance")
+print(evaluate(reg, test_x, test_y))
+
+print("I thiink I did better than a dummy regressor!")
+
+# %%
